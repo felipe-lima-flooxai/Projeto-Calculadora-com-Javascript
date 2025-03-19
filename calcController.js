799c44467c79bcaf79b2d18c454995c7e06e7ba1
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-03-19 08:31:11 -0300

    feat: add keyboard key events

--- calcController.js
+++ calcController.js
@@ -10,6 +10,7 @@ class CalcControlller {
         this.currentDate;
         this.initialize(); 
         this.initButtonsEvents();
+        this.initKeyboard();
     }
 
     initialize(){
@@ -18,6 +19,58 @@ class CalcControlller {
             this.setDisplayDateTime();
        }, 1000);
         this.setLastNumberToDisplay();
+    }
+
+    initKeyboard(){
+        document.addEventListener('keyup', e => {
+            console.log(e.key);
+
+            switch(e.key){
+                case 'Escape':
+                    this.clearAll();
+                    break;
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+                case '+':
+                    this.addOperation('+');
+                    break;
+                case '-':
+                    this.addOperation('-');
+                    break;
+                case '/':
+                    this.addOperation('/');
+                    break;
+                case '*':
+                    this.addOperation('*');
+                    break;
+                case '%':
+                    this.addOperation('%');
+                    break;
+                case '=':
+                    this.calc();
+                    break;  
+                case 'Enter':
+                    this.calc();
+                    break;
+                case '.':
+                    this.addDot();
+                    break;
+    
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;                           
+            }
+        });
     }
 
     setDisplayDateTime(){
@@ -114,7 +167,7 @@ class CalcControlller {
     setLastNumberToDisplay(){
         let lastNumber = this.getLastItem(false);
         
-        if(!lastNumber){
+        if(!lastNumber && lastNumber != '.'){
             lastNumber = 0;
         }
         this.displayCalc = lastNumber;
